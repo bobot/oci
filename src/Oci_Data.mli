b09269f9233b14f0d5c5f7af33c71119b0c6f3a3
François Bobot <francois.bobot@cea.fr>
2016-02-26 15:43:45 +0100

    Simplify Oci_Log API

--- src/Oci_Data.mli
+++ src/Oci_Data.mli
@@ -39,12 +39,12 @@ exception NoResult
 val name: ('query,'result) t -> string
 val version: ('query,'result) t -> int
 val rpc: ('query,'result) t -> ('query,'result Or_error.t) Rpc.Rpc.t
-(** return the first data of the log, or raise {!NoResult} if none *)
+(* return the first data of the log, or raise {!NoResult} if none *)
 val log: ('query,'result) t ->
   ('query, 'result Oci_Log.line, Error.t) Rpc.Pipe_rpc.t
 val forget: ('query,'result) t -> ('query, unit Or_error.t) Rpc.Rpc.t
-(** If the 'query are memoized forget the previous computation.
-    does nothing if there is nothing to forget *)
+(* If the 'query are memoized forget the previous computation.
+   does nothing if there is nothing to forget *)
 
 
 val id:   ('query,'result) t -> ('query * 'result) Type_equal.Id.t
